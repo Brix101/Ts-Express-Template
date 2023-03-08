f4e4e225c0db824c396ed726c1a765906575f58c
Brixter Porras <brixterporras@gmail.com>
2023-03-08 15:39:24 +0800

    chore(env): remove comments & updated env example

--- src/modules/routes.interface.ts
+++ src/modules/routes.interface.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+// Todo move this interface
 export interface Routes {
   path?: string;
   router: Router;
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,6 @@
 import App from "core/app";
 import modules from "modules";
-// import { generateKeyPairSync } from "crypto";
 
-// const { privateKey, publicKey } = generateKeyPairSync("rsa", {
-//   modulusLength: 2048,
-// });
-
-// console.log(
-//   privateKey.export({
-//     format: "pem",
-//     type: "pkcs1",
-//   }),
-//   publicKey.export({
-//     format: "pem",
-//     type: "pkcs1",
-//   })
-// );
 modules.then((routes) => {
   const app = new App(routes);
 
